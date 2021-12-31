65373dc0eddbb684f20e557fc1419daf94bbec60
Knut Tomasson Joramo <knut.joramo@gmail.com>
2021-12-31 14:27:52 +0100

    update name logic to player

--- player.py
+++ player.py
@@ -10,4 +10,10 @@ class Player:
     def __init__(self,  player_name=""):
         Player.player_count += 1
         self.player_id = Player.player_count
-        
+        name_holder = player_name.strip()
+        self.player_name = ""
+        if(name_holder == ""):
+            self.player_name = "Unknown player nr.{}".format(self.player_id)
+        else:
+            self.player_name = player_name
+
