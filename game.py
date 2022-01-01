e151713e07d8aac0d49edeb4690104274fbc7a14
Knut Tomasson Joramo <knut.joramo@gmail.com>
2022-01-01 17:37:06 +0100

    more work on game and add deck file/class

--- game.py
+++ game.py
@@ -3,11 +3,22 @@
 # implements the logic of the game
 # #
 
-import player, dealer
+from player import Player
+from dealer import Dealer
 
 class Game:
-    def __init__(self, dealer, players=[]):
-        self.dealer = dealer
+    def __init__(self, players=[]):
+        self.dealer = Dealer()
         self.players_playing = players
         self.busted_players = []
         self.standing_players = []
+
+    def add_player(self, player):
+        print("Pleas write player name or exit to abort:")
+        name = input()
+        if name != "exit":
+            self.players_playing.append(Player(name))
+
+
+    def run(self):
+        pass
